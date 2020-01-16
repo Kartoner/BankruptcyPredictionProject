27299fbbd6345e16e5e03bbf13b2743f6d2ee977
kartoner <jakub.michal.ostrowski@gmail.com>
2020-01-16 18:59:56 +0100

    Upload project

---
+++ src/main/java/jo/BankruptcyPredictionProject/BankruptcyPredictionProjectApplication.java
@@ -0,0 +1,13 @@
+package jo.BankruptcyPredictionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankruptcyPredictionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankruptcyPredictionProjectApplication.class, args);
+	}
+
+}
